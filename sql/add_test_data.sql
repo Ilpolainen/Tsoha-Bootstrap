4b41db48ac753e2d13e97fe2d1344990227f5aa8
Pohjola Ilmari V <vipohjol@da5-cs-b221-20.helsinki.fi>
2016-05-09 16:15:50 +0300

    tää olikin päivän myöhässä

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,3 +1,10 @@
-DELETE FROM Kiinnostustagi WHERE id = 3;
 
--- Lisää INSERT INTO lauseet tähän tiedostoon
+
+-- Lisää INSERT INTO lauseet tähän tiedost
+INSERT INTO Kayttaja (etunimi, sukunimi, kayttajatunnus, salasana, syntyma_aika, osoite, puhelinnumero, email_osoite, kuvaus) VALUES ('Ilpo', 'Mäkelä', 'Pultsari', 'potkukelkka', '1989-03-03', 'Maalarintie 5', '0000000', 'ilpo@ilpo.com', 'olen kiltti mies');
+INSERT INTO Tapahtuma (tapahtuman_nimi, tapahtumapaikka, lyhyt_kuvaus, pvm, kellonaika, tapahtuman_luoja) VALUES ('Ruokakerho', 'Turun keittiö', 'Laitetaan ruokaa yhdessä', '2016-12-12', '12:00' , 1);
+INSERT INTO Kiinnostustagi (kiinnostus) VALUES ('rumpujensoitto');
+
+INSERT INTO Kayttajan_kiinnostus (kayttaja, kiinnostus) VALUES (1, 1);
+INSERT INTO Osallistuminen (kayttaja_id, tapahtuma_id) VALUES (1, 1);
+INSERT INTO Tapahtuman_aihe (tapahtuma, aihe) VALUES (1, 1);
